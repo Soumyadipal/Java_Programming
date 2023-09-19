a06aa77e0b7770c9aad373563e15800bc193a475
soumyadipal <soumyadipal08@gmail.com>
2023-09-20 03:18:19 +0530

    First Update

--- Sorting/src/MergeSort.java
+++ Sorting/src/MergeSort.java
@@ -1,5 +1,12 @@
 public class MergeSort {
+    static void mergeSorting(int [] arr) {
+
+    }
     public static void main(String[] args) {
-        System.out.println("Soumyadip");
+        int [] arr = {8,3,6,2,4,5};
+        mergeSorting(arr);
+        System.out.print("The sorted array is : ");
+        for (int i = 0;i < arr.length;i++)
+            System.out.print(arr[i] + " ");
     }
 }
